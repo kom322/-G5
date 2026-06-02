ff407353a1fff0ea9b54c82db1116c9128c537bb
ＣＨＡ Ｓｅ ｈｙｕｎ(is0645he) <is0645he@ed.ritsumei.ac.jp>
2026-06-02 14:36:56 +0900

    　style: comment delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-// 💡 수정 포인트 1: 가짜 데이터(Mock Data)에 아마존 가격(Yahoo)을 추가했습니다.
 const mockProductHistory = [
   { date: '2026-05-28', Amazon: 499.99, Rakuten: 450.00, Yahoo: 480.00 },
   { date: '2026-05-29', Amazon: 499.99, Rakuten: 460.00, Yahoo: 475.00 },
@@ -14,9 +13,9 @@ function App() {
   return (
     <div style={{ backgroundColor: 'white', color: 'black', padding: '40px', width: '800px', margin: '50px auto', border: '2px solid #ccc', borderRadius: '12px' }}>
       
-      <h1 style={{ textAlign: 'center', marginBottom: '30px' }}>📈 PlayStation 5 쇼핑몰 가격 비교</h1>
+      <h1 style={{ textAlign: 'center', marginBottom: '30px' }}>📈 PlayStation 5 価格</h1>
       
-      {/* 3개 쇼핑몰을 보여주기 위해 너비를 700에서 750으로 살짝 늘렸습니다 */}
+
       <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'center' }}>
         <LineChart width={750} height={400} data={mockProductHistory}>
           <CartesianGrid strokeDasharray="3 3" />
@@ -27,8 +26,6 @@ function App() {
           
           <Line type="monotone" dataKey="Amazon" name="Amazon" stroke="#0046be" strokeWidth={3} />
           <Line type="monotone" dataKey="Rakuten" name="Rakuten" stroke="#e53238" strokeWidth={3} />
-          
-          {/* 💡 수정 포인트 2: 아마존(Amazon)을 뜻하는 새로운 꺾은선(Line) 부품을 추가했습니다. (색상은 아마존의 상징인 주황색!) */}
           <Line type="monotone" dataKey="Yahoo" name="Yahoo" stroke="#ff9900" strokeWidth={3} />
           
         </LineChart>
